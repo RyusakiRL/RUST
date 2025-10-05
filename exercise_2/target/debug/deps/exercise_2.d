896ce65de43e0cc9dda608bf06b896bc9ee4ab7b
Caio Kazu <caioitaya08@gmail.com>
2025-10-04 23:11:52 -0400

    Exercise 2

---
+++ exercise_2/target/debug/deps/exercise_2.d
@@ -0,0 +1,5 @@
+C:\Users\Caio\hello_rust\exercise_2\target\debug\deps\exercise_2.d: src\main.rs
+
+C:\Users\Caio\hello_rust\exercise_2\target\debug\deps\exercise_2.exe: src\main.rs
+
+src\main.rs:
---
+++ exercise_2/target/debug/exercise_2.d
@@ -0,0 +1,1 @@
+C:\Users\Caio\hello_rust\exercise_2\target\debug\exercise_2.exe: C:\Users\Caio\hello_rust\exercise_2\src\main.rs
