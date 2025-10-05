36c154a96141f96d94350edc3a8aa81c278d49ed
Caio Kazu <caioitaya08@gmail.com>
2025-10-05 12:25:29 -0400

    Determine the value of delta in quadratic equations

--- rust-fundamentals-exercises/ex_10/src/main.rs
+++ rust-fundamentals-exercises/ex_10/src/main.rs
@@ -1,20 +1,26 @@
 use std::io;
 
-fn main (){
+    fn main(){
+        println!("Write the value of first variable");
+        let mut a = String::new();
+        io::stdin().read_line(&mut a)
+        .expect("msg");
 
-        println!("Write the width of a wall");
-    let mut w = String::new();
-    io::stdin().read_line(&mut w)
-    .expect("msg");
+        println!("Write the value of second variable");
+        let mut b = String::new();
+        io::stdin().read_line(&mut b)
+        .expect("msg");
 
-        println!("Write the height of the same wall");
-    let mut h = String::new();
-    io::stdin().read_line(&mut h)
-    .expect("msg");
+        println!("Write the value of third variable");
+        let mut c = String::new();
+        io::stdin().read_line(&mut c)
+        .expect("msg");
 
-            let ww: f64 = w.trim().parse().expect("msg");
-            let hh: f64 = h.trim().parse().expect("msg");
+            let aa: f64 = a.trim().parse().expect("msg");
+            let bb: f64 = b.trim().parse().expect("msg");
+            let cc: f64 = c.trim().parse().expect("msg");
 
-            let a = hh*ww;
-        println!("Area of wall is {}, necesssary {} bucket of ink", a, a/2.0 );
-}
+        let delta = bb*bb -4.0*aa*cc;
+
+            println!("The value of delta of the quadratic equation {}", delta);
+    }
